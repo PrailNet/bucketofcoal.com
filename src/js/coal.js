5af67cf9a9a602405896e8466264dfbaf842c6df
Noah Prail <nprail@filiosoft.com>
2018-12-17 09:46:37 -0500

    chore(codestyle): Update code style

--- src/js/coal.js
+++ src/js/coal.js
@@ -1,72 +1,74 @@
-(function($) {
-  "use strict"; // Start of use strict
+/* global Stripe jQuery */
+
+;(function ($) {
+  'use strict' // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
+      window.location.pathname.replace(/^\//, '') ===
+        this.pathname.replace(/^\//, '') &&
+      window.location.hostname === this.hostname
     ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
       if (target.length) {
-        $("html, body").animate(
+        $('html, body').animate(
           {
             scrollTop: target.offset().top - 70
           },
           1000,
-          "easeInOutExpo"
-        );
-        return false;
+          'easeInOutExpo'
+        )
+        return false
       }
     }
-  });
+  })
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function() {
-    $(".navbar-collapse").collapse("hide");
-  });
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide')
+  })
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $("body").scrollspy({
-    target: "#mainNav",
+  $('body').scrollspy({
+    target: '#mainNav',
     offset: 100
-  });
+  })
 
   // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
+  var navbarCollapse = function () {
+    if ($('#mainNav').offset().top > 100) {
+      $('#mainNav').addClass('navbar-shrink')
     } else {
-      $("#mainNav").removeClass("navbar-shrink");
+      $('#mainNav').removeClass('navbar-shrink')
     }
-  };
+  }
   // Collapse now if page is not at top
-  navbarCollapse();
+  navbarCollapse()
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  $(window).scroll(navbarCollapse)
 
-  var stripe = Stripe("pk_live_qqoVVEdeWc49isA4t568IDqL", {
-    betas: ["checkout_beta_4"]
-  });
+  var stripe = Stripe('pk_live_qqoVVEdeWc49isA4t568IDqL', {
+    betas: ['checkout_beta_4']
+  })
 
-  $("#orderForm").submit(function(event) {
-    event.preventDefault();
+  $('#orderForm').submit(function (event) {
+    event.preventDefault()
 
-    var size = $("#size").val();
+    var size = $('#size').val()
 
     stripe
       .redirectToCheckout({
         items: [{ sku: size, quantity: 1 }],
-        successUrl: "https://bucketofcoal.com/success",
-        cancelUrl: "https://bucketofcoal.com/canceled"
+        successUrl: 'https://bucketofcoal.com/success',
+        cancelUrl: 'https://bucketofcoal.com/canceled'
       })
-      .then(function(result) {
+      .then(function (result) {
         if (result.error) {
-          var displayError = document.getElementById("error-message");
-          displayError.textContent = result.error.message;
+          var displayError = document.getElementById('error-message')
+          displayError.textContent = result.error.message
         }
-      });
-  });
-})(jQuery); // End of use strict
+      })
+  })
+})(jQuery) // End of use strict
